44cb10e699e98a97ce37c8a6a042bd64cf442ce9
dwuhasjik <ksjciara.matteo@hotmail.com>
2024-05-08 21:28:01 +0700

    init project

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
+function canJump(nums) {
+  let maxJump = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxJump) return false;
+    maxJump = Math.max(maxJump, i + nums[i]);
+    if (maxJump >= nums.length - 1) return true;
   }
-  return arr;
+  return false;
 }
