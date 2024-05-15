0a09f8b3ca56b7530b5a24b60533bf5241bfd7ab
dwuhasjik <ksjciara.matteo@hotmail.com>
2024-05-15 14:28:04 +0700

    start commit

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-function rob(nums) {
-  let prevMax = 0;
-  let currMax = 0;
-  for (const num of nums) {
-    const temp = currMax;
-    currMax = Math.max(prevMax + num, currMax);
-    prevMax = temp;
-  }
-  return currMax;
+function pathSum(root, sum) {
+  if (!root) return false;
+  if (!root.left && !root.right && root.val === sum) return true;
+  return (
+    pathSum(root.left, sum - root.val) || pathSum(root.right, sum - root.val)
+  );
 }
